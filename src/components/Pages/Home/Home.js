203793398cb2dcc209dc9b788fee25c764aa8097
DPSDR <dipta751@gmail.com>
2022-11-13 01:42:56 +0600

    set the home coomponent

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>COX'S BAZAR</h1>
-            <p>Cox's Bazar is a city, fishing port, tourism centre and famous mostly for its long natural sandy beach and it ...</p>
-            <button className="btn btn-warning">Booking</button>
-        </div>
+        <div className='container mx-auto'>
+            <div className='w-50 text-white text-start home-intro'>
+                <h1>COX'S BAZAR</h1>
+                <p className='mt-3'>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                <button className="btn btn-warning px-4 my-3 fw-bolder">
+                    Booking
+                </button>
+            </div>
+            <div className=''></div>
+        </div >
     );
 };
 
